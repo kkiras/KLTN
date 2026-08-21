21fa1bdf30e5d4f7813864131d9d6c72ce656433
Masou9999 <hatiendu123@gmail.com>
2026-08-21 17:48:22 +0700

    Cập nhật logic Card, cập nhật chức năng rút các là bài khác nhau,  sửa lại các scripts: card.cs, carddisplay.cs

--- Assets/Scripts/CardDisplay.cs
+++ Assets/Scripts/CardDisplay.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -10,16 +12,31 @@ public class CardDisplay : MonoBehaviour
     public Image cardImage;
     public TMP_Text healthText;
     public TMP_Text damageText;
-    
+    public Image[] typeImages;
     void Start()
     {
-        UpdateCardDisplay();    
+        UpdateCardDisplay();
     }
 
     public void UpdateCardDisplay()
     {
         healthText.text = cardData.health.ToString();
         damageText.text = cardData.damage.ToString();
+
+        // //Update type images
+        for (int i = 0; i < typeImages.Length; i++)
+        {
+            typeImages[i].gameObject.SetActive(false);
+        }
+        for (int i = 0; i < cardData.cardType.Count; i++)
+        {
+            Card.CardType type = cardData.cardType[i];
+            int typeIndex = (int)type;
+            if (typeIndex < typeImages.Length)
+            {
+                typeImages[typeIndex].gameObject.SetActive(true);
+            }
+        }
     }
-    
+
 }
